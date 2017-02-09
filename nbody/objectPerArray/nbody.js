c4b021d74c35bafab74cb63ba36661f4e2066f49
andrews <andrewstrain@pericosm.com>
2017-02-09 12:57:00 +0000

    tidy with comments

--- nbody/objectPerArray/nbody.js
+++ nbody/objectPerArray/nbody.js
@@ -4,18 +4,18 @@
    modified by Andrew Strain */
 
 var PI = 3.141592653589793;
-var SOLAR_MASS = 4 * PI * PI;
-var DAYS_PER_YEAR = 365.24;
+var MASS_FACTOR = 4 * PI * PI;  //not really PI related
+var TIME_FACTOR = 365.24;       //not really days either
 
 var body={
-     'top'  : 0 //
-    ,'x'    : [-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,] 
-    ,'y'    : [-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,]
-    ,'z'    : [-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,]
-    ,'vx'   : [-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,]
-    ,'vy'   : [-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,]
-    ,'vz'   : [-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,]
-    ,'mass' : [-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,-0,]
+     'top'  : 0 
+    ,'x'    : [-0,-0,-0,-0,-0] 
+    ,'y'    : [-0,-0,-0,-0,-0]
+    ,'z'    : [-0,-0,-0,-0,-0]
+    ,'vx'   : [-0,-0,-0,-0,-0]
+    ,'vy'   : [-0,-0,-0,-0,-0]
+    ,'vz'   : [-0,-0,-0,-0,-0]
+    ,'mass' : [-0,-0,-0,-0,-0]
 }
 
 function addBody(x,y,z,vx,vy,vz,mass){
@@ -32,10 +32,10 @@ function addBody(x,y,z,vx,vy,vz,mass){
    
 }
 
-function offsetSunMomentum(px,py,pz){
-   body.vx[0] = -px / SOLAR_MASS;
-   body.vy[0] = -py / SOLAR_MASS;
-   body.vz[0] = -pz / SOLAR_MASS;
+function sunMomentumMagic(px,py,pz){
+   body.vx[0] = -px / MASS_FACTOR;
+   body.vy[0] = -py / MASS_FACTOR;
+   body.vz[0] = -pz / MASS_FACTOR;
 }
 
 function Jupiter(){
@@ -43,10 +43,10 @@ function Jupiter(){
       4.84143144246472090e+00,
       -1.16032004402742839e+00,
       -1.03622044471123109e-01,
-      1.66007664274403694e-03 * DAYS_PER_YEAR,
-      7.69901118419740425e-03 * DAYS_PER_YEAR,
-      -6.90460016972063023e-05 * DAYS_PER_YEAR,
-      9.54791938424326609e-04 * SOLAR_MASS
+      1.66007664274403694e-03 * TIME_FACTOR,
+      7.69901118419740425e-03 * TIME_FACTOR,
+      -6.90460016972063023e-05 * TIME_FACTOR,
+      9.54791938424326609e-04 * MASS_FACTOR
    );
 }
 
@@ -55,10 +55,10 @@ function Saturn(){
       8.34336671824457987e+00,
       4.12479856412430479e+00,
       -4.03523417114321381e-01,
-      -2.76742510726862411e-03 * DAYS_PER_YEAR,
-      4.99852801234917238e-03 * DAYS_PER_YEAR,
-      2.30417297573763929e-05 * DAYS_PER_YEAR,
-      2.85885980666130812e-04 * SOLAR_MASS
+      -2.76742510726862411e-03 * TIME_FACTOR,
+      4.99852801234917238e-03 * TIME_FACTOR,
+      2.30417297573763929e-05 * TIME_FACTOR,
+      2.85885980666130812e-04 * MASS_FACTOR
    );
 }
 
@@ -67,10 +67,10 @@ function Uranus(){
       1.28943695621391310e+01,
       -1.51111514016986312e+01,
       -2.23307578892655734e-01,
-      2.96460137564761618e-03 * DAYS_PER_YEAR,
-      2.37847173959480950e-03 * DAYS_PER_YEAR,
-      -2.96589568540237556e-05 * DAYS_PER_YEAR,
-      4.36624404335156298e-05 * SOLAR_MASS
+      2.96460137564761618e-03 * TIME_FACTOR,
+      2.37847173959480950e-03 * TIME_FACTOR,
+      -2.96589568540237556e-05 * TIME_FACTOR,
+      4.36624404335156298e-05 * MASS_FACTOR
    );
 }
 
@@ -79,15 +79,15 @@ function Neptune(){
       1.53796971148509165e+01,
       -2.59193146099879641e+01,
       1.79258772950371181e-01,
-      2.68067772490389322e-03 * DAYS_PER_YEAR,
-      1.62824170038242295e-03 * DAYS_PER_YEAR,
-      -9.51592254519715870e-05 * DAYS_PER_YEAR,
-      5.15138902046611451e-05 * SOLAR_MASS
+      2.68067772490389322e-03 * TIME_FACTOR,
+      1.62824170038242295e-03 * TIME_FACTOR,
+      -9.51592254519715870e-05 * TIME_FACTOR,
+      5.15138902046611451e-05 * MASS_FACTOR
    );
 }
 
 function Sun(){
-   return new addBody(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, SOLAR_MASS);
+   return new addBody(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, MASS_FACTOR);
 }
 
 
@@ -102,7 +102,7 @@ function NBodySystem(){
       py += body.vy[i] * m;
       pz += body.vz[i] * m;
    }
-   offsetSunMomentum(px,py,pz);
+   sunMomentumMagic(px,py,pz);
 }
 
 function advanceBodies(dt){
@@ -159,7 +159,7 @@ function energyOfBodies(){
    return e;
 }
 
-Sun(),Jupiter(),Saturn(),Uranus(),Neptune()
+Sun(),Jupiter(),Saturn(),Uranus(),Neptune() //load celestials
 
 function runTest(n) { 
   var bodies = new NBodySystem();
